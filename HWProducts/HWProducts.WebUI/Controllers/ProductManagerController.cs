7f8632f3d6eb6645039085af71c69cd46c129331
kaulamit84 <amitkaul190184@gmail.com>
2019-10-09 23:00:37 -0400

    Fixed problems in WebUI Tests

---
+++ HWProducts/HWProducts.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,25 @@
+﻿using HWProducts.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HWProducts.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
